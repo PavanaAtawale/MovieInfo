9fac50e9ee6b85532051514cffe3b6c397fb6594
PavanaAtawale <pavana.atawale@gmail.com>
2024-08-27 14:45:49 -0700

    Fixes #2

--- imdb.js
+++ imdb.js
@@ -7,36 +7,56 @@ const options = {
 	}
 };
 
+async function showResult() {
+  document.querySelector("#search_phrase").style.setProperty("display", "block");
+  const results = document.querySelectorAll(".result");
+  results.forEach(r => r.style.setProperty("display", "block"));
+} 
+
+async function hideResult() {
+  document.querySelector("#search_phrase").style.setProperty("display", "none");
+  const results = document.querySelectorAll(".result");
+  results.forEach(r => r.style.setProperty("display", "none"));
+  document.querySelector("#error").style.setProperty("display", "none");
+}
+
+async function showError(e) {
+  document.querySelector("#search_phrase").style.setProperty("display", "block");
+  document.querySelector("#error").style.setProperty("display", "block");
+  document.querySelector("#error").innerHTML = e;
+}
+
 async function search(keyword) {
   try {
     let str = url + keyword;
     const response = await fetch(str, options);
     const result = await response.json();
     console.log(result);
-    const results = document.querySelectorAll(".result");
-    results.forEach(r => r.style.setProperty("display", "block"));
-    document.querySelector("#title").innerHTML = result.data[0].title;
-    document.querySelector("#cast").innerHTML = result.data[0].stars;
-    document.querySelector("#year").innerHTML = result.data[0].year;
+    if(result.status ==  true) {
+      document.querySelector("#title").innerHTML = result.data[0].title;
+      document.querySelector("#cast").innerHTML = result.data[0].stars;
+      document.querySelector("#year").innerHTML = result.data[0].year;
+      showResult();
+    } else {
+      hideResult();
+      showError(result.message);
+    }
+    
   } catch (error) {
     console.error(error);
   }
 }
 
 
-
-
 window.onload = function(){
-    const results = document.querySelectorAll(".result");
-    results.forEach(r => r.style.setProperty("display", "none"));
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const data = new FormData(e.target);
-        const sp = data.get("movie");
-        //console.log(sp);
-        document.querySelector("#search_phrase").innerHTML = "You searched for <strong>'" + sp + "'</strong>";
-        search(sp);
-        form.reset();
-      });
+  hideResult();
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const sp = data.get("movie");
+    search(sp);
+    document.querySelector("#search_phrase").innerHTML = "You searched for <strong>'" + sp + "'</strong>";
+    form.reset();
+  });
 }
